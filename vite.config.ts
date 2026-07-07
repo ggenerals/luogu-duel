9246668d9e9bbd5cc98c8a5d7c104c0f07b1aa68
liyifan202201 <liyifan202201@outlook.com>
2026-07-07 13:14:32 +0800

    添加Cloudflare Rocket Loader禁用功能

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  plugins: [
+    {
+      name: "cloudflare-rocket-loader-optout",
+      transformIndexHtml(html) {
+        return html.replaceAll("<script type=\"module\"", "<script data-cfasync=\"false\" type=\"module\"");
+      }
+    }
+  ],
   server: {
     allowedHosts: ["hn.frp.one"]
   }
